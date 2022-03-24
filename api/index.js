015997b3fb62c23048999360f9d91b632346c4d6
sharp211 <95362611+sharp211@users.noreply.github.com>
2022-03-24 23:53:22 +0800

    feat: changed server domain

--- api/index.js
+++ api/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const baseDonmain = 'http://localhost:1337';
+const baseDonmain = 'https://riode-vue.herokuapp.com';
 
 export const customHeader = {
     'Content-Type': 'application/json',
     Accept: 'application/json'
 };
 
-export const baseUrl = `${ baseDonmain }`;
+export const baseUrl = `${baseDonmain}`;
 export const currentDemo = "29";
 
-export default axios.create( {
+export default axios.create({
     baseUrl,
     headers: customHeader
-} )
+})
